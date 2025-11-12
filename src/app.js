d8ec53c66cb17b365170994d12f1665a11254212
farrel irawan <irawanssfarrel@gmail.com>
2025-11-12 15:36:01 +0700

    feat: make server bootstrap and server app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import authRoute from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use(cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/v1", authRoute);
+
+export { app };
