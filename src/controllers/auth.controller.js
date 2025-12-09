584894de76445d3f7e6514cc7bd0c392859b5b00
farrel irawan <irawanssfarrel@gmail.com>
2025-12-09 20:51:51 +0700

    feat: #86evtw53e Handle auth system both on client side and server side
    * feat: add access and refresh token cookies in signUpController
    
    * feat: update CORS middleware configuration to include origin and credentials

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,6 +29,23 @@ export const signUpController = async (req, res, next) => {
   }
 
   const result = await signUpService(signUpData.data);
+
+  const { access_token, refresh_token } = result.session;
+
+  res.cookie("accessToken", access_token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refresh_token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
   const payload = successPayload(result);
 
   res.json({
