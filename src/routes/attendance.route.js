515da46810a73b7ce8fd08121aa5edfcf6a81785
farrel irawan <irawanssfarrel@gmail.com>
2025-12-18 19:11:50 +0700

    feat: implement get all attendance route as an API endpoint

--- src/routes/attendance.route.js
+++ src/routes/attendance.route.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 import { storeAttendanceController } from "../controllers/storeAttendance.controller.js";
+import { getAllAttendanceController } from "../controllers/getAllAttendance.controller.js";
 
 const route = Router();
 
-route.post("attendance/store", verifyToken, storeAttendanceController);
+route.get("/attendance", verifyToken, getAllAttendanceController);
+route.post("/attendance/store", verifyToken, storeAttendanceController);
 
 export default route;
