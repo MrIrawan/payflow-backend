ad3466e011cb30f20fe76c6e628afd23beb5779a
farrel irawan <irawanssfarrel@gmail.com>
2025-12-19 23:20:07 +0700

    feat: changing iso to date utils function into format date utils function

--- src/utils/isoToDate.js
+++ src/utils/isoToDate.js
@@ -1,5 +1,5 @@
-export function isoToDate(isoString) {
-  const date = new Date(isoString).toLocaleDateString("id-ID", {
+export function formatDate(dateString) {
+  const date = new Date(dateString).toLocaleDateString("en-US", {
     day: "2-digit",
     month: "long",
     year: "numeric",
