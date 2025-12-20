6e4911a6ab302ea220ad4b79594406e14f198625
farrel irawan <irawanssfarrel@gmail.com>
2025-12-20 14:29:04 +0700

    feat: implement updateAttendance service and controller

--- src/routes/attendance.route.js
+++ src/routes/attendance.route.js
@@ -3,10 +3,12 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 import { storeAttendanceController } from "../controllers/storeAttendance.controller.js";
 import { getAllAttendanceController } from "../controllers/getAllAttendance.controller.js";
+import { updateAttendanceController } from "../controllers/updateAttendance.controller.js";
 
 const route = Router();
 
 route.get("/attendance", getAllAttendanceController);
 route.post("/attendance/store", storeAttendanceController);
+route.put("/attendance/update/:attendance_id", updateAttendanceController);
 
 export default route;
