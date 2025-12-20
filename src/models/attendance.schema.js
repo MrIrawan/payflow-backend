9bc8b021783e269bbb049a70dd6669ca9a746c2f
farrel irawan <irawanssfarrel@gmail.com>
2025-12-20 17:20:22 +0700

    feat: change teacher_id into teacher_name on attendanceSchema

--- src/models/attendance.schema.js
+++ src/models/attendance.schema.js
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const attendanceSchema = z.object({
-  teacher_id: z.string("teacher id is required"),
+  teacher_name: z.string("teacher name is required"),
   attendance_date: z.coerce.date("attendance date is required."),
   checkin_time: z.number("check in time is required"),
   checkout_time: z.number("check out time is required"),
