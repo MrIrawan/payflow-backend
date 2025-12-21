bc30475eaea264e9168f6c9a763b802a8b129c65
farrel irawan <irawanssfarrel@gmail.com>
2025-12-21 18:54:00 +0700

    adding get attendance by date controller into attendance route as an API endpoint

--- src/routes/attendance.route.js
+++ src/routes/attendance.route.js
@@ -5,10 +5,12 @@ import { storeAttendanceController } from "../controllers/attendance.controller.
 import { updateAttendanceController } from "../controllers/attendance.controller.js";
 import { getAllAttendanceController } from "../controllers/attendance.controller.js";
 import { deleteAttendanceController } from "../controllers/attendance.controller.js";
+import { getAttendanceByDateController } from "../controllers/attendance.controller.js";
 
 const route = Router();
 
 route.get("/attendance", getAllAttendanceController);
+route.get("attendance/date", getAttendanceByDateController);
 route.post("/attendance/store", storeAttendanceController);
 route.put("/attendance/update/:attendance_id", updateAttendanceController);
 route.delete("/attendance/delete/:attendance_id", deleteAttendanceController);
