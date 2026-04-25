2a8467b4a25e606ea5804911401df24a9d19d12c
MrIrawan <irawanssfarrel@gmail.com>
2026-04-25 11:43:50 +0700

    fix: update addNewCompany schema and service to include owner_id and optional fields

--- src/services/user/company/addNewCompany.service.js
+++ src/services/user/company/addNewCompany.service.js
@@ -1,13 +1,16 @@
-import { supabase } from "../../../lib/supabase.js";
+import { supabaseAdmin } from "../../../lib/supabaseAdmin.js";
 
 export async function addNewCompanyService(data) {
     if (!data) {
         throw new Error("Data is required");
     }
 
-    const newCompany = await supabase
-        .from("company-list")
-        .insert(data)
+    const newCompany = await supabaseAdmin
+        .from("companies")
+        .insert({
+            owner_id: data.owner_id,
+            ...data
+        })
         .select()
         .single();
 
