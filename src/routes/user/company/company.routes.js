ebd0f828db5295076ec604cdb01537889061b04b
MrIrawan <irawanssfarrel@gmail.com>
2026-08-16 19:30:08 +0700

    fix: adding company switch route on company routes file and deleting company switch route on employee routes file

--- src/routes/user/company/company.routes.js
+++ src/routes/user/company/company.routes.js
@@ -4,6 +4,7 @@ import { addNewCompanyController } from '../../../controllers/user/company/addNe
 import { getOwnCompanyController } from '../../../controllers/user/company/getOwnCompany.controller.js';
 import { editOwnCompanyController } from '../../../controllers/user/company/editOwnCompany.controller.js';
 import { deleteOwnCompanyController } from '../../../controllers/user/company/deleteOwnCompany.controller.js';
+import { switchCompanyController } from "../../../controllers/user/employee/switchCompany.controller.js";
 
 import { isUserAuthenticated } from '../../../middleware/isAuthenticated.js';
 import { joinCompanyController } from '../../../controllers/user/company/joinCompany.controller.js';
@@ -12,8 +13,10 @@ const router = express.Router();
 
 router.post('/company/add', isUserAuthenticated, addNewCompanyController);
 router.post('/company/join', isUserAuthenticated, joinCompanyController);
+router.post("/company/switch", isUserAuthenticated, switchCompanyController);
 router.get('/company', isUserAuthenticated, getOwnCompanyController);
 router.put("/company/edit/:companyId", isUserAuthenticated, editOwnCompanyController);
 router.delete("/company/delete/:companyId", isUserAuthenticated, deleteOwnCompanyController);
+
 
 export default router;
--- src/routes/user/employee/employee.route.js
+++ src/routes/user/employee/employee.route.js
@@ -1,13 +1,11 @@
 import express from "express";
 
 import { getAllEmployeesOnCompanyController } from "../../../controllers/user/employee/getAllEmployeesOnCompany.js";
-import { switchCompanyController } from "../../../controllers/user/employee/switchCompany.controller.js";
 
 import { isUserAuthenticated } from "../../../middleware/isAuthenticated.js";
 
 const route = express.Router();
 
 route.get("/employees", isUserAuthenticated, getAllEmployeesOnCompanyController);
-route.post("/company/switch", isUserAuthenticated, switchCompanyController);
 
 export default route;
