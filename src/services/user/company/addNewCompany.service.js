e6747c702d0bce78d50d62bb16b94a27030c9612
farrel irawan <irawanssfarrel@gmail.com>
2026-08-31 19:58:29 +0700

    feat: Implement File Upload Handling for Company Avatar (#10)
    * chore: installing two dependencies for file handling (multer and sharp)
    
    * feat: creating file upload handling middleware function with multer
    
    * feat: creating image compressor middleware function using sharp
    
    * feat: implement file upload using supabase storage on add new company service
    
    * fix: adding folder prefix on fileName variable on add new company service
    
    * fix: fixing error throw on add new company query
    
    * feat: adding return statement on error validation on add new company controller

--- src/services/user/company/addNewCompany.service.js
+++ src/services/user/company/addNewCompany.service.js
@@ -1,24 +1,43 @@
 import { supabaseAdmin } from "../../../lib/supabaseAdmin.js";
-import { mergeName } from "../../../utils/mergeName.js";
 
-export async function addNewCompanyService(identifier, data) {
+export async function addNewCompanyService(identifier, data, file) {
+    let avatarUrl = null;
+
     if (!data) {
         throw new Error("Data is required");
     }
+
+    if (file) {
+        const fileName = `company-avatars/company-avatar-${identifier}-${Date.now()}.webp`;
+
+        const { error: uploadError } = await supabaseAdmin.storage
+            .from("payflow-assets")
+            .upload(fileName, file.buffer, {
+                contentType: "image/webp",
+                upsert: false
+            });
+
+        if (uploadError) throw uploadError;
+
+        const { data: uploadUrl } = await supabaseAdmin.storage
+            .from("payflow-assets")
+            .getPublicUrl(fileName);
+
+        avatarUrl = uploadUrl.publicUrl;
+    }
+
 
     const newCompanyQuery = await supabaseAdmin
         .from("companies")
         .insert({
             owner_id: identifier,
+            avatar_url: avatarUrl,
             ...data
         })
         .select()
         .single();
 
-    if (newCompanyQuery.error) {
-        console.error("Error adding new company:", newCompanyQuery.error);
-        throw new Error("Error adding new company.");
-    }
+    if (newCompanyQuery.error) throw newCompanyQuery.error;
 
     return newCompanyQuery;
 };
